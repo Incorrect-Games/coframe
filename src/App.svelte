df11bcd47f692a1ab06ef446bea5ade91146aa02
Dyrhoi <realarcoz@gmail.com>
2021-09-24 21:59:49 +0100

    Removed Svelte Warning

--- src/App.svelte
+++ src/App.svelte
@@ -57,6 +57,7 @@
 	<div>
 		<input type="file" on:change={changeVideo} />
 		<br />
+		<!-- svelte-ignore a11y-media-has-caption -->
 		<video bind:this={VIDEO_PLAYER} controls src={videoSrc} />
 	</div>
 	<br />
@@ -83,13 +84,6 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
